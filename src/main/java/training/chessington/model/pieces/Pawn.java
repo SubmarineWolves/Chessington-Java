660e46323c861d8b06c6cc2bc0a7ddc856179e37
BROWN Samuel <sabrown@europe.corp.altran.com>
2018-09-27 11:10:44 +0100

    Test 5 done

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -16,28 +16,30 @@ public class Pawn extends AbstractPiece {
 	@Override
 	public List<Move> getAllowedMoves(Coordinates from, Board board) {
 
-		int dir = 1;
-		int origin = 1;
+		int dir;
+		int origin;
 		if (this.getColour().equals(PlayerColour.WHITE)) {
 			dir = -1;
 			origin = 6;
+		} else {
+			dir = 1;
+			origin = 1;
 		}
 
 		ArrayList<Move> moves = new ArrayList<>();
 
-		Coordinates to = new Coordinates(from.getRow() + dir, from.getCol());
-		if(board.isEmpty(to))
-		{
-		moves.add(new Move(from, to));
-		if (from.getRow()==origin && board.isEmpty(to.plus(dir,0))) {
-			moves.add(new Move(from, new Coordinates(from.getRow() + (2*dir), from.getCol())));
+		if(from.getRow() !=7 && from.getRow() != 0) {
+			Coordinates to = from.plus(dir, 0);
+
+			if(board.isEmpty(to))
+			{
+				moves.add(new Move(from, to));
+				if (from.getRow()==origin && board.isEmpty(to.plus(dir,0))) {
+					moves.add(new Move(from, new Coordinates(from.getRow() + (2*dir), from.getCol())));
+				}
+			}
 		}
-		}
-
-
-
-
-
 		return moves;
+
 	}
 }
